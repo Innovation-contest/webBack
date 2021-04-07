9810731a08c98fa9bcb4021e6f8308c81de67c45
baixi123 <1426221905@qq.com>
2021-04-07 09:28:27 +0800

    some pojo

---
+++ src/java/Process.java
@@ -0,0 +1,13 @@
+package java;
+
+import java.util.List;
+
+public class Process {
+    private int id;
+    private Boolean parallel;
+    private String workspace;
+    private int max_num;
+    private List<Object> resource;
+    private int exec_time;
+    private int end_time;
+}
